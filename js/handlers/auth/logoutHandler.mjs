278a39b719807f68029cc297c056c473ce06d465
Johan Frederic Aas-Haug <91760515+Zarden92@users.noreply.github.com>
2024-07-24 14:04:17 +0200

    added logout handler, removing username, password and token before redirecting to login page

---
+++ js/handlers/auth/logoutHandler.mjs
@@ -0,0 +1,25 @@
+import { remove } from "../../utilities/storage/index.mjs";
+
+/**
+ * Handles the logout process.
+ * When the logout button is clicked, the access token is removed from storage and the user is redirected to the home page.
+ */
+
+export function logoutHandler() {
+  const logoutButton = document.querySelector("#logoutBtn");
+
+  logoutButton.addEventListener("click", handleLogout);
+
+  function handleLogout(event) {
+    event.preventDefault();
+
+    console.log("Logging out...");
+
+    remove("token");
+    remove("userName");
+    remove("email");
+
+    console.log("localstorage cleared..");
+    window.location.href = "/";
+  }
+}
--- js/script.mjs
+++ js/script.mjs
@@ -3,6 +3,7 @@ console.log("Here we add the switch function router");
 
 import { registerFormHandler } from "./handlers/auth/registerFormHandler.mjs";
 import { loginFormHandler } from "./handlers/auth/loginFormHandler.mjs";
+import { logoutHandler } from "./handlers/auth/logoutHandler.mjs";
 
 function router() {
   const pathname = window.location.pathname;
@@ -19,6 +20,11 @@ function router() {
     case "/register/index.html":
       registerFormHandler();
       console.log("This is the registration page");
+      break;
+    case "/feed/":
+    case "/feed/index.html":
+      logoutHandler();
+      console.log("This is the feed page");
       break;
   }
 }
